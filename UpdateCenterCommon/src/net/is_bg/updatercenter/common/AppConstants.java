240b62f2df30514c69586ae068240fd05d0baf87
lkaymakanov <bat_lubo2001@abv.bg>
2015-12-11 17:28:46 +0200

    Create test client to update apps!

--- UpdateCenterCommon/src/net/is_bg/updatercenter/common/AppConstants.java
+++ UpdateCenterCommon/src/net/is_bg/updatercenter/common/AppConstants.java
@@ -2,27 +2,12 @@ package net.is_bg.updatercenter.common;
 
 public class AppConstants {
 	
-	
-	
 	// REST PATHS
 	public final static String PATH_LTF = "/ltf";
 	public final static String PATH_TEST = "/test";
 	public final static String PATH_APPLICATION ="/app";
 	
-	//REST SUBPATHS
-	public final static String SUBPATH_UPDATE_CLIENT_INFO = "/updateclientinfo";
-	public final static String SUBPATH_CURRENT_VERSION = "/currentversion";
-	public final static String SUBPATH_GET_CHUNK_NO = "/getchunkno";
-	public final static String SUBPATH_GET_SESSION = "/getsession";
-	public final static String SUBPATH_GET_LIB = "/lib";
-	public static final String SUBPATH_GET_FILE = "/filename";
-	public static final String SUBPATH_IS_SESSION_ACTIVE = "/sessionactive";
-	// REST PARAMS
-	public final static String PARAM_CHUNK_NO = "chunkNo";
-	public final static String PARAM_SESSION_ID = "sessionId";
-	public final static String PARAM_FILE_NAME = "fileName";
-	//PATH PARAMS
-	public final static String PATH_ARAM_CHUNK_NO = "/{chunkNo}";
+	public final static String PARAM_SESSION_ID = "sessionid";
 	
 	
 }
--- UpdateCenterCommon/src/net/is_bg/updatercenter/common/Enumerators.java
+++ UpdateCenterCommon/src/net/is_bg/updatercenter/common/Enumerators.java
@@ -6,14 +6,12 @@ import com.cc.rest.client.enumerators.IREST_PATH;
 public class Enumerators {
 	public enum REST_PATH implements IREST_PATH{
 		TEST(AppConstants.PATH_TEST),
-		LTF(AppConstants.PATH_LTF),
-		CURRENT_VERSION(AppConstants.SUBPATH_CURRENT_VERSION),
-		GET_CHUNK_NO(AppConstants.SUBPATH_GET_CHUNK_NO),
-		UPDATE_CLIENT_INFO(AppConstants.SUBPATH_UPDATE_CLIENT_INFO),
-		GET_SESSION(AppConstants.SUBPATH_GET_SESSION),
-		GET_FILE(AppConstants.SUBPATH_GET_FILE),
-		IS_SESSION_ACTIVE(AppConstants.SUBPATH_IS_SESSION_ACTIVE);
+		APP(AppConstants.PATH_APPLICATION),
+		LTF(AppConstants.PATH_LTF);
+		
+		
 		private String path;
+		
 		private REST_PATH(String path){
 			this.path = path;
 		}
@@ -21,12 +19,14 @@ public class Enumerators {
 		public String getPath() {
 			return path;
 		}
+		public void setPath(String path) {
+			this.path = path;
+		}
 	}
 	
+	
 	public enum PARAMS implements IPARAM {
-		CHUNK_NO(AppConstants.PARAM_CHUNK_NO),
-		SESSION_ID(AppConstants.PARAM_SESSION_ID),
-		FILE_NAME(AppConstants.PARAM_FILE_NAME);
+		SESSION_ID(AppConstants.PARAM_SESSION_ID);
 		private PARAMS(String stringValue){
 			this.stringValue = stringValue;
 		}
@@ -43,4 +43,57 @@ public class Enumerators {
 		INACTIVE,
 		SERVER_BUSY;
 	}
+	
+	
+	public static IREST_PATH getVersionSubPath(String appName){
+		SELECTIVE_PATHS p = new  SELECTIVE_PATHS();
+		p.setPath("/" + appName + "/versioninfo/");    
+		return p;
+	}
+	
+	public static IREST_PATH getFileSubPath(String appName, String fName){
+		SELECTIVE_PATHS p = new  SELECTIVE_PATHS();
+		p.setPath("/" + appName + "/file/" + fName);
+		return p;
+	}
+	
+	
+	public static IREST_PATH getCreateSessionSubPath(String appName){
+		SELECTIVE_PATHS p = new  SELECTIVE_PATHS();
+		p.setPath("/" + appName + "/session/");
+		return p;
+	}
+	
+	public static IREST_PATH getSessionsSubPath(){
+		SELECTIVE_PATHS p = new  SELECTIVE_PATHS();
+		p.setPath("/sessions/");
+		return p;
+	}
+	
+	public static IREST_PATH getUpdatesSubPath(){
+		SELECTIVE_PATHS p = new  SELECTIVE_PATHS();
+		p.setPath("/updates/");
+		return p;
+	}
+	
+	public static IREST_PATH getLibsSubPath(){
+		SELECTIVE_PATHS p = new  SELECTIVE_PATHS();
+		p.setPath("/libs/");
+		return p;
+	}
+	
+	
+	public static class SELECTIVE_PATHS implements IREST_PATH{
+		private String path;
+		void setPath(String path){
+			this.path = path;
+		}
+		
+		@Override
+		public String getPath() {
+			// TODO Auto-generated method stub
+			return path;
+		}
+	}
+	
 }
