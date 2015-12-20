6c37a491f9202ba3021e36b91b0a6b84379f3ebc
lkaymakanov <bat_lubo2001@abv.bg>
2015-12-20 22:41:01 +0200

    changes

--- UpdateCenterServer/JavaSource/update/center/manager/ManageVersionBean.java
+++ UpdateCenterServer/JavaSource/update/center/manager/ManageVersionBean.java
@@ -1,7 +1,29 @@
 package update.center.manager;
 
+import java.util.Collection;
+import java.util.Set;
+
+import update.center.controllers.IUpdateCenterController;
+import update.center.controllers.UpdateCenterController;
+import version.VersionDescriptions;
+import version.WarVersionDescription;
+
 
 
 public class ManageVersionBean {
 
+	private IUpdateCenterController c = UpdateCenterController.getIUpdateCenterController();
+	
+	public String getLibs(){
+		return c.getLibraries().toString();
+	}
+	
+	
+	public Collection<WarVersionDescription> getVersionDescriptions(){
+		return VersionDescriptions.getDescriptions();
+	}
+	
+	public Set<String> getSessions() {
+		return c.getSessions();
+	}
 }
